f932a97ba6b81489f88aef129a5fb90b3a7f4791
Jack Parker <jprkr96@gmail.com>
2025-09-04 23:47:55 -0500

    Stuff for fixing relative paths

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
-#include "progress_bar.h"
+#include "progress_bar.cpp"
 #include <iostream>
 
-int main (int argc, char *argv[]) {
+int main(int argc, char *argv[]) {
   std::cout << "Hello World!" << std::endl;
   return 0;
 }
--- src/progress_bar.cpp
+++ src/progress_bar.cpp
@@ -1,20 +1,19 @@
-#include "progress_bar.h"
-#include <iostream>
-#include <fstream>
+#include "../include/progress_bar.h"
 #include <chrono>
-#include <thread>
+#include <iostream>
 #include <string>
+#include <thread>
 
-void process_info(int process_time){
-    std::this_thread::sleep_for(std::chrono::milliseconds(process_time));
+void process_info(int process_time) {
+  std::this_thread::sleep_for(std::chrono::milliseconds(process_time));
 }
-void print_progress_bar(double progress){
+void print_progress_bar(double progress) {
   int bar_length = 50;
   int filled_length = static_cast<int>(progress * bar_length);
   std::string barChar = "#";
   std::cout << "[";
 
-  for(int i = 0; i < filled_length; i++){
+  for (int i = 0; i < filled_length; i++) {
     std::cout << barChar;
   }
   for (int i = filled_length; i < bar_length; i++) {
